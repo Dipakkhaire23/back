6e52c6143744a4fc4c4c04004832dcc1d25219ef
Dipakkhaire23 <dipakkhaire23@gmail.com>
2025-03-02 11:08:20 +0530

    vchjfg

--- src/main/java/com/alpha/portfolio/PortfolioApplication.java
+++ src/main/java/com/alpha/portfolio/PortfolioApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
+	public static void main(String[]   arg) {
+		SpringApplication.run(PortfolioApplication.class, arg);
+		
 	}
 
 }
