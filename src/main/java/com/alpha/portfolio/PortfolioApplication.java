e65602ada9ae3da60757019c59ac5c82a1d717a1
Dipakkhaire23 <dipakkhaire23@gmail.com>
2025-03-02 11:13:10 +0530

    fivechanges

--- src/main/java/com/alpha/portfolio/PortfolioApplication.java
+++ src/main/java/com/alpha/portfolio/PortfolioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioApplication {
 
-	public static void main(String[]   arg) {
-		SpringApplication.run(PortfolioApplication.class, arg);
+	public static void main(String[]   args) {
+		SpringApplication.run(PortfolioApplication.class, args);
 		
 	}
 
